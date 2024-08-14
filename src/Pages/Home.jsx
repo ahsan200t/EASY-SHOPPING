3762f76110889726327ad399e6304a3bb3c82ab0
ahsan200t <ahsanpekua200@gmail.com>
2024-08-14 23:38:59 +0600

    Footer Done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
-        <div className="container mx-auto">
-            <Navbar/>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <Navbar />
+      <div className="h-[100vh]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
